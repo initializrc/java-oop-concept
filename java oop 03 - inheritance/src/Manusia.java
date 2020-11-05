2b19ab3ee3ee4c25318ff8db4b5145fbd14c9cba
initializrc <panji.cahyountoro@gmail.com>
2020-11-05 08:45:55 +0700

    inheritance

---
+++ java oop 03 - inheritance/src/Manusia.java
@@ -0,0 +1,30 @@
+public class Manusia {
+
+    private String nama;
+    private int umur;
+
+    public Manusia(String nama, int umur) {
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    public void ulangTahun(){
+        umur++;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+}
