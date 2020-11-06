781473d1b4d5aeaee92639efbf7ebb902990543e
initializrc <panji.cahyountoro@gmail.com>
2020-11-06 08:36:57 +0700

    inheritance

--- java oop 03 - inheritance/src/Main.java
+++ java oop 03 - inheritance/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * parent class tidak dapat mengakses child class
  * child class dapat meengakses semua milik parent class
@@ -26,5 +28,49 @@ public class Main {
         // class parent tidak bisa mengakses method dari class child
         // saitama.belajar();
 
+        // tidak dapat membuat object parent class (Manusia) dan tampung di child class (Mahasiswa)
+        // Mahasiswa allMighty = new Manusia("All Mighty", 20);
+
+        // ArrayList dari class
+        ArrayList<Mahasiswa> listMahasiswa = new ArrayList<Mahasiswa>();
+        listMahasiswa.add(budi);
+        listMahasiswa.add(new Mahasiswa("All Mighty", 20, "444658769", 3.92f));
+
+        System.out.println(budi); // hanya print alamat stack
+        System.out.println(budi.getNama()); // print nama saja
+        // print satu data
+        System.out.println("Nama : " + budi.getNama() + " " +
+                           "Umur : " + budi.getUmur() + " " +
+                           "NIM : " + budi.getNim() + " " +
+                           "IPK : " + budi.getIpk());
+
+        System.out.println("\n" + listMahasiswa.get(1)); // hanya print alamat stack
+        System.out.println(listMahasiswa.get(1).getNama()); // print nama saja
+        // print satu data
+        System.out.println("Nama : " + listMahasiswa.get(1).getNama() + " " +
+                           "Umur : " + listMahasiswa.get(1).getUmur() + " " +
+                           "NIM : " + listMahasiswa.get(1).getNim() + " " +
+                           "IPK : " + listMahasiswa.get(1).getIpk());
+
+        System.out.println();
+
+        // print semua data menggunakan for ada dua cara
+        // cara 1
+        System.out.println();
+        for (int i = 0; i < listMahasiswa.size(); i++){
+            System.out.println(listMahasiswa.get(i).getNama() + " " +
+                               listMahasiswa.get(i).getUmur() + " " +
+                               listMahasiswa.get(i).getNim() + " " +
+                               listMahasiswa.get(i).getIpk()) ;
+        }
+
+        // cara 2
+        System.out.println("\nPrint Seluruh Data ArrayList");
+        for (Mahasiswa mahasiswa : listMahasiswa){
+            System.out.println("Nama : " + mahasiswa.getNama() + " " +
+                               "Umur : " + mahasiswa.getUmur() + " " +
+                               "NIM : " + mahasiswa.getNim() + " " +
+                               "IPK : " + mahasiswa.getIpk());
+        }
     }
 }
