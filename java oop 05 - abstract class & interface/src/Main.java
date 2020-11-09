0c1bd489379bd4d0f62197370020ee683da47853
initializrc <panji.cahyountoro@gmail.com>
2020-11-09 15:49:18 +0700

    abstract class & interface

--- java oop 05 - abstract class & interface/src/Main.java
+++ java oop 05 - abstract class & interface/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        // error akan terjadi jika dibuat object dari abstract class
 
+        // buat object dari class turunannya
+        Cat cat = new Cat();
+
+        // panggil method yang sudah dioverride dari abstract method
+        cat.makeNoise();
+
+        // membuat object Wolf dan ditampung di abstract class
+        Animal obj = new Wolf();
+        obj.makeNoise();
     }
 }
