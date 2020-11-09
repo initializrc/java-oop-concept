965aa865925662fac19a47a712527ef194521ed9
initializrc <panji.cahyountoro@gmail.com>
2020-11-09 15:54:38 +0700

    abstract class & interface

---
+++ java oop 05 - abstract class & interface/src/InterfaceSample.java
@@ -0,0 +1,12 @@
+public interface InterfaceSample {
+
+    // variable  ini sudah static final
+    int number = 100;
+
+    // method merupakan public abstract
+    // hanya kepalanya saja (only head)
+    // tidak bisa pakai body
+    public void interfaceMethod1();
+    public void interfaceMethod2();
+    public void interfaceMethod3();
+}
