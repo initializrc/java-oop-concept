f7aecbbc30a129e9b351035b88eb69f14739559d
initializrc <panji.cahyountoro@gmail.com>
2020-11-10 05:49:52 +0700

    generic programming

---
+++ java oop 06 - generic programming/src/GenericClass.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class GenericClass<E>{
+    ArrayList<E> list = new ArrayList<E>();
+
+    // untuk menambah data
+    public void push(E e){
+        list.add(e);
+    }
+
+    // untuk mengambil jumlah data
+    public int getSize(){
+        return list.size();
+    }
+
+    // untuk menghapus data
+    public E pop(){
+        E e = list.get(getSize() - 1);
+        list.remove(getSize() - 1);
+
+        return e;
+    }
+}
